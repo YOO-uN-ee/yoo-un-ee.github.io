07dedc90a459bbe187c3dc4dae5185f4de0bce00
YOO-uN-ee <jiyoonp0228@gmail.com>
2026-01-29 23:54:57 -0600

    updated statics

--- src/data/static.ts
+++ src/data/static.ts
@@ -1,27 +1,21 @@
 export const staticPubs = [
 	{
-		title: '1',
-		authors: 'Jiyoon Pyo, Second Author, Third Author',
-		venue: 'Preprint',
-		year: 2025,
+		title: 'Leveraging Large Language Models for Generating Labeled Mineral Site Record Linkage Data',
+		authors: 'Jiyoon Pyo, Yao-Yi Chiang',
+		venue: 'Proceedings of the 7th ACM SIGSPATIAL International Workshop on AI for Geographic Knowledge Discovery',
+		year: 2024,
 		links: {
-			paper: 'https://arxiv.org/abs/xxxx.xxxxx',
-			webpage: 'https://example.com',
-			github: 'https://github.com/org/repo',
-			bibtex: '/bib/fer.bib'
+			paper: 'https://doi.org/10.1145/3687123.3698298',
+			github: 'https://github.com/DARPA-CRITICALMAAS/umn-ta2-mineral-site-linkage',
 		}
 	},
 	{
-		title: '2',
-		authors: 'Jiyoon Pyo, Second Author, Third Author',
-		venue: 'Preprint',
+		title: 'Detection and replacement of neologisms for translation',
+		authors: 'Jiyoon Pyo',
+		venue: 'The Cooper Union for the Advancement of Science and Art',
 		year: 2024,
 		links: {
-			paper: 'https://arxiv.org/abs/xxxx.xxxxx',
-			webpage: 'https://example.com',
-			github: 'https://github.com/org/repo',
-			huggingface: 'https://huggingface.co/org/model',
-			bibtex: '/bib/fer.bib'
+			paper: 'https://www.proquest.com/openview/1be717392643ea691fa02b3821920bd7/1?pq-origsite=gscholar&cbl=18750&diss=y#/',
 		}
 	}
 ]
