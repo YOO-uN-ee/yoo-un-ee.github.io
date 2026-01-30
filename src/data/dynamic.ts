bed33ba10287b29a6446e3f1d596e3e594647f62
YOO-uN-ee <jiyoonp0228@gmail.com>
2026-01-29 23:47:52 -0600

    storing data

---
+++ src/data/dynamic.ts
@@ -0,0 +1,27 @@
+export const staticPubs = [
+	{
+		title: '3',
+		authors: 'Jiyoon Pyo, Second Author, Third Author',
+		venue: 'Preprint',
+		year: 2025,
+		links: {
+			paper: 'https://arxiv.org/abs/xxxx.xxxxx',
+			webpage: 'https://example.com',
+			github: 'https://github.com/org/repo',
+			bibtex: '/bib/fer.bib'
+		}
+	},
+	{
+		title: '4',
+		authors: 'Jiyoon Pyo, Second Author, Third Author',
+		venue: 'Preprint',
+		year: 2024,
+		links: {
+			paper: 'https://arxiv.org/abs/xxxx.xxxxx',
+			webpage: 'https://example.com',
+			github: 'https://github.com/org/repo',
+			huggingface: 'https://huggingface.co/org/model',
+			bibtex: '/bib/fer.bib'
+		}
+	}
+]
